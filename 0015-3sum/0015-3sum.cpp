9bf69e8615ea8aa8d1489ec7b6b4a9b65a5a79cd
Md Mizanur rahman <mizanmd92782@gmail.com>
2023-08-21 21:39:37 +0600

    Time: 1144 ms (24.19%), Space: 191.5 MB (14.77%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        
+        set<vector<int>>set;
+       sort(arr.begin(),arr.end());
+       for(int i=0;i<arr.size()-2;i++)
+       {
+            int x=-arr[i];
+            int l=i+1;
+            int r=arr.size()-1;
+            int k=0;
+            while(l<r)
+            {
+               
+             
+
+                  
+
+                 if(arr[r]+arr[l]==x)
+                 {
+                     
+                     set.insert({arr[i],arr[l],arr[r]});
+                     l++;
+                     r--;
+                    
+                 }
+
+
+                 if(arr[r]+arr[l]>x)  r--;
+                 if(arr[r]+arr[l]<x) l++;
+                
+            }
+       }
+
+
+      
+      vector<vector<int>>vec;
+      for(auto x:set)
+      {
+           vec.push_back(x);
+      }
+
+
+       return vec;
+    }
+};
