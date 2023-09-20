5d5121641b805ab59568b9348f0f35b3b10674ad
Md Mizanur rahman <mizanmd92782@gmail.com>
2023-09-21 02:04:06 +0600

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -10,34 +10,40 @@ class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    int lcs(int n, int m, string s1, string s2)
+    int lcs(int m, int n, string s1, string s2)
     {
-        int dp[1001][1001];
+         int dpTab[m+1][n+1];
+     
+     for(int i=0;i<=m;i++)
+     {
+         for(int j= 0;j<=n;j++)
+         {
+             
+
+                if(i==0 || j==0)
+                {
+                    dpTab[i][j]=0;
+
+                }else if (s1[i-1]==s2[j-1])
+                {
+                      
+                      dpTab[i][j]=1+dpTab[i-1][j-1];
+                      
+                }else{
+
+                    dpTab[i][j]=max(dpTab[i-1][j],dpTab[i][j-1]);
+                }
+
+               
+
+
+         }
+
         
-        //Following steps build dp[x+1][y+1] in bottom up fashion. Note that 
-        //dp[i][j] contains length of LCS of s1[0..i-1] and s2[0..j-1].
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                //if i or j is 0, we mark dp[i][j] as 0.
-                if(i==0||j==0)
-                dp[i][j]=0;     
-                
-                //else if the current char in both strings are equal, we add 1 
-                //to the output we got without including these both characters.
-                else if (s1[i-1] == s2[j-1])        
-                dp[i][j] = dp[i-1][j-1] + 1; 
-                
-                //else s1[i-1]!=s2[j-1] so we check the max output which 
-                //comes from s1 or s2 without considering current character.
-                else
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]); 
-            }
-        }
-        
-        //returning the result.
-        return dp[n][m];  
+     }
+
+
+     return dpTab[m][n];
          
     }
 };
