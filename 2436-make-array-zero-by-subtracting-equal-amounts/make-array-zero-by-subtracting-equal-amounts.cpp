eca2ba2f655ba1b378123cb53c8fe86cc9ba6874
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-01-28 01:31:30 +0600

    Time: 5 ms (22.55%) | Memory: 10.1 MB (8.40%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
 
-        set<int>s;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]!=0)  s.insert(nums[i]);
-        }
+        set<int>s(nums.begin(),nums.end());
+        
         
 
-        return s.size();
+        return s.size()-s.count(0);
     }
 };
