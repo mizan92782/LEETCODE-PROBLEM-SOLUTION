72fcaf4e17fcdcabcaefa2e54e0b57d2726c8620
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-01-31 23:49:02 +0600

    Time: 66 ms (10.48%) | Memory: 12.7 MB (12.36%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
-        priority_queue<int>p;
-        for(int i=0;i<gifts.size();i++)
-        {
-            p.push(gifts[i]);
-        }
-        while(k)
-        {
-            int temp=p.top();
-            p.pop();
-            p.push(sqrt(temp));
-            k--;
-        }
-        long long ans=0;
-        while(!p.empty())
-        {
-            ans+=p.top();
-            p.pop();
-        }
-        return ans;
+       
+       sort(gifts.begin(),gifts.end(),greater<int>());
+       int n=gifts.size();
+
+       int i=0;
+       while(k--)
+       {
+           gifts[i]=sqrt(gifts[i]);
+            sort(gifts.begin(),gifts.end(),greater<int>());
+        
+       }
+
+
+      long long sum=0;
+      for(int i=0;i<n;i++)
+      {
+          sum=sum+gifts[i];
+      }
+
+
+      return sum;
+
     }
+
+       
 };
