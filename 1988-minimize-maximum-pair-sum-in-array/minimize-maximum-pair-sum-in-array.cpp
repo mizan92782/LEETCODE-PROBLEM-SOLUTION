9ee59ff02220cc74d1b923e426934e5451e2a8d3
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-02-22 15:09:05 +0600

    Time: 227 ms (11.75%) | Memory: 99.8 MB (27.20%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -1,3 +1,11 @@
+
+const static auto fast = [] {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
 class Solution {
 public:
     int minPairSum(vector<int>& nums) {
