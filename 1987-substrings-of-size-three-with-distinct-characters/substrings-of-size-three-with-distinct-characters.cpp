db258bbb8a3233e367efb1fd499829668ef49135
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-02-26 11:22:33 +0600

    Time: 3 ms (58.94%) | Memory: 7.4 MB (49.58%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int countGoodSubstrings(string s) {
         int count=0;
-        if(s.size()<3) return count;
-        for(int i=0;i<=s.size()-3;i++)
+        int n=s.size();
+        if(n<3) return count;
+
+
+
+        for(int i=0;i<=n-3;i++)
         {
             if(s[i]!=s[i+1] && s[i]!=s[i+2] && s[i+1]!=s[i+2]) 
             {
                 count++;
-                cout<<s[i]<<s[i+1]<<s[i+2]<<endl;
-               
             }
         }
 
