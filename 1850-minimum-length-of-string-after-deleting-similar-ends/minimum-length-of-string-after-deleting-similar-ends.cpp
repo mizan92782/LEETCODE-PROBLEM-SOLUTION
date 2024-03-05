641044b34f7dbcb32bc3fe03f1bfa8cfb0e81914
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-03-05 18:34:35 +0600

    Time: 395 ms (5.17%) | Memory: 13.9 MB (44.14%) - LeetSync

---
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+    
+      int n=s.size();
+      int i=0;
+      int j=n-1;
+
+      char c=',';
+
+     int count=0;
+
+     
+      while(i<j)
+      {
+         
+          if(s[i]==s[j])
+          {
+            
+             
+              count=count+2;
+               cout<<i<<" -> "<<j<<"->>> "<<count<<endl;
+               c=s[i];
+
+             i++;
+             j--;
+            
+          }else if(s[i]==c)
+          {  
+              
+               count++;
+                cout<<i<<" -> "<<j<< " ->>> "<<count<<endl;
+             
+              i++;
+          }else if(s[j]==c){
+              
+              count++;
+               cout<<i<<" -> "<<j<< " ->>> "<<count<<endl;
+              j--;
+          }else{
+              break;
+          }
+          
+      }
+    
+     if(c==s[i] && i==j)
+     {
+         count++;
+     }
+
+       
+
+      
+
+   
+     return s.size()-count;
+        
+    }
+};
