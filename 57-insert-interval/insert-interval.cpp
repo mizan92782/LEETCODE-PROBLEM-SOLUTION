177017622cf94e44f6cae16e246d3e34db310bc2
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-03-17 21:54:30 +0600

    Time: 11 ms (65.36%) | Memory: 20.4 MB (82.99%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,3 +1,12 @@
+const static auto fast = [] {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+
+
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
