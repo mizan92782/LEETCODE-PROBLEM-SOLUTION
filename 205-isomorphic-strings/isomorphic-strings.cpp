395c8b28bb22dd07adb562b1269c4a44c145ecf7
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-04-02 11:27:40 +0600

    Time: 12 ms (12.32%) | Memory: 10.8 MB (5.05%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,3 +1,13 @@
+const static auto fast = [] {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+
+
+
 class Solution {
 public:
     struct mapping{
