17520e505ac3bc4df5307fde1c5f87c65432420f
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-09-14 00:35:34 +0600

    Time: 15 ms (92.28%) | Memory: 79.9 MB (15.11%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -8,97 +8,80 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- const static auto fast = [] {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
-
-
-
-
-static const auto _ = [](){
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    return 0;
-}();
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* first, ListNode* second) {
+
+
+  
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        stack<ListNode*>one;
-        stack<ListNode*>two;
+
+        stack<int>s1;
+         while(l1)
+         {
+           s1.push(l1->val);
+           l1=l1->next;
+
+         }
+
+
+         stack<int>s2;
+         while(l2)
+         {
+           s2.push(l2->val);
+           l2=l2->next;
+
+         }
+
+
         
-        while(first)
-        {
-            one.push(first);
-            first=first->next;
-        }
+       
         
-        while(second)
-        {
-             two.push(second);
-            second=second->next;
-        }
-        
-        
-        ListNode* newhead=NULL;
-        
-        
-        
-        int x,y;
-        int rem=0;
-        int sum;
-        
-        while(!one.empty() || !two.empty())
-        {    
-              x=0;
-              y=0;
-              
-             if(!one.empty())
-             {
-                 x=one.top()->val;
-                 one.pop();
+
+         ListNode* ans= new ListNode(0);
+
+         while(!s1.empty() || !s2.empty())
+         {
+            int sum=0;
+
+             if(!s1.empty()){
+                 sum=sum+s1.top();
+                 s1.pop();
              }
-             
-             
-             if(!two.empty())
-             {
-                 y=two.top()->val;
-                 two.pop();
+
+
+             if(!s2.empty()){
+                 sum=sum+s2.top();
+                 s2.pop();
              }
-             
-             
-             sum=x+y+rem;
-              if(newhead==NULL)
-              {
-                  newhead= new ListNode(sum%10);
-              }else
-              {
-                   ListNode* temp =new ListNode(sum%10);
-                   temp->next=newhead;
-                   newhead=temp;
-             
-              }
-             
-              rem=sum/10;
-              
-             
-        }
-        
-        
-        if(rem!=0)
-        {
-             ListNode* temp= new ListNode(rem);
-             temp->next=newhead;
-             newhead=temp;
-        }
-        
-        
-        
-        return  newhead;
-        
+
+
+
+               
+                int x=(sum+ans->val);
+
+                ans->val=x%10;
+                x=x/10;
+
+
+                ListNode* head = new ListNode(x);
+                head->next=ans;
+                ans=head;
+
+
+         }
+
+
+
+          if(ans->val==0) ans=ans->next;
+         return ans;
+
+
+
+
+
+
+
 
     }
 };
