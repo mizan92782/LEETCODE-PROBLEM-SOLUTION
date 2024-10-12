3fc81d7861f7a7ff85f3c9f0a66cb918c4105c56
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-10-13 00:53:27 +0600

    Time: 32 ms (11.96%) | Memory: 25.7 MB (5.15%) - LeetSync

---
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+ bool overlap(vector<int>a,vector<int>b)
+ {
+    return (a[0]<=b[1] && b[0]<= a[1]);
+ }
+
+ vector<int> intersection(vector<int>a,vector<int>b)
+ {
+    return {max(a[0],b[0]),min(a[1],b[1])};
+ }
+
+
+ vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+       
+       int fn=firstList.size();
+       int sn=secondList.size();
+        vector<vector<int>> result;
+       
+        int one=0;
+        int two=0; 
+        while(one<fn && two<sn){
+         
+               if(overlap(firstList[one],secondList[two]))
+               {
+                   result.push_back(intersection(firstList[one],secondList[two]));
+                   
+                  
+               }
+
+
+                 if(firstList[one][1]>secondList[two][1])
+                   {
+                     two++;
+                   }else if(firstList[one][1]<secondList[two][1])
+                   {
+                     one++;
+                   }else
+                   {
+                     one++;
+                     two++;
+                   }
+               
+          
+       }
+
+
+
+       return result;
+    }
+};
