c1d7a2d3b97d6118ab728015ed7354069d363a0c
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-10-20 00:04:15 +0600

    Time: 1889 ms (16.15%) | Memory: 509.3 MB (16.16%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
 
-    static bool comp(vector<int> a, vector<int> b)
-   {
-    return a[1]<b[1];
-   }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        
-        sort(intervals.begin(),intervals.end(),comp);
+         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        sort(intervals.begin(),intervals.end(),[](vector<int>a,vector<int>b){
+            return a[1]<b[1];
+        });
 
       
 
