1cf741d47b51dc94e7dccffb0dea1b52073db7ad
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-12-07 02:36:54 +0600

    Time: 22 ms (21.17%) | Memory: 58.6 MB (18.77%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        
+
+       int n=num1.size()-1;
+       int m=num2.size()-1;
+       string str="";
+
+      int carry=0;
+       
+       while(n>=0 || m>=0)
+       {
+           int a=0;
+           int b=0;
+
+           if(n>=0)
+           {
+             a=num1[n]-'0';
+             n--;
+           }
+
+
+           if(m>=0)
+           {
+            b=num2[m]-'0';
+            m--;
+           }
+
+         
+           carry=a+b+carry;
+           char c=char(carry%10 +'0');
+           carry=carry/10;
+
+
+           str=c+str;
+
+
+       }
+
+
+       if(carry!=0){
+         str=char(carry+'0')+str;
+       }
+
+
+       return str;
+
+
+    }
+};
