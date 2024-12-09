8c9f00948b43beccf22263d762ab67c1139d2864
Md Mizanur Rahman <mizanmd92782@gmail.com>
2024-12-10 01:18:30 +0600

    Time: 1078 ms (5.47%) | Memory: 126.6 MB (63.34%) - LeetSync

---
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        
+
+        int n=nums.size();
+
+        vector<int>mem(n,0);
+
+        int count=0;
+        for(int i=1;i<n;i++)
+        {
+             if(nums[i]%2 == nums[i-1]%2)
+             {
+               count++;
+             }
+
+             mem[i]=count;
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            cout<<mem[i]<<" ";
+        }
+
+        int m=queries.size();
+        vector<bool>ans(m,false);
+
+        for(int i=0;i<queries.size();i++)
+        {
+                ans[i]= (mem[queries[i][1]]-mem[queries[i][0]])==0;
+                cout<<mem[queries[i][1]] << " -> "<<mem[queries[i][0]];
+                
+        }
+
+
+        return  ans;
+    }
+};
