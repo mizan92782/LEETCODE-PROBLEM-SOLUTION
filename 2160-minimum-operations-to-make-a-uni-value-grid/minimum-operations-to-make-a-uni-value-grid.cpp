2a09a20959734b9512f7cf955107dfcedce01801
Md Mizanur Rahman <mizanmd92782@gmail.com>
2025-03-27 03:24:06 +0600

    Time: 43 ms (56.02%) | Memory: 90.2 MB (35.54%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,84 +1,35 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        
-        int miv=INT_MAX;
-        int mxv=INT_MIN;
-
-        vector<int>vec;
-
-        
-
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[i].size();j++)
-            {
-                int xv=grid[i][j];
-
-                if(xv<miv) miv=xv;
-                if(xv>mxv) mxv=xv;
-
-                vec.push_back(xv);
-
-                
-                
-            }
+        vector<int>arr;
+        // Flatten the grid into a 1D array
+    for (const auto& row : grid) {
+        for (int num : row) {
+            arr.push_back(num);
         }
+    }
 
+    // Sort the array
+    sort(arr.begin(), arr.end());
 
-     
-
-      int n=vec.size();
-       
-        //mxv==miv
-        if(mxv==miv) return 0;
-        
-        //if not possible
-        int dif=mxv-miv;
-        if(dif%x!=0) return -1;
-
-        //eeveno dd case
-
-      
-
-
-        sort(vec.begin(),vec.end());
-        
-
-
-       
-        vector<pair<int,int>>result(n);
-
-
-        result[0].first=0;
-
-        for(int i=1;i<n;i++)
-        {   
-            if((vec[i]-vec[i-1])%x!=0) return -1;
-            result[i].first=((vec[i]-vec[i-1])/x)*(i)+result[i-1].first;
+    // Check if all elements can be converted to the same value (divisibility check)
+    int base = arr[0] % x;
+    for (int num : arr) {
+        if (num % x != base) {
+            return -1;  // Not possible to make all elements equal
         }
+    }
 
-          
-        result[n-1].second=0;
-        int a=1;
-        for(int i=n-2;i>=0;i--)
-        { 
-            
-            result[i].second=((vec[i+1]-vec[i])/x)*(a)+result[i+1].second;
-            a++;
-        }
+    // Choose the median as the target value
+    int median = arr[arr.size() / 2];
 
-        
-        int count=INT_MAX;
-        for(int i=0;i<n;i++)
-        {
-            count=min(count, result[i].second+ result[i].first);
-        }
-        
+    // Calculate the minimum operations needed
+    int operations = 0;
+    for (int num : arr) {
+        operations += abs(num - median) / x;
+    }
 
-    
-
-     return count;
+    return operations;
 
     }
 };
